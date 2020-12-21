54a19cf47d81cbcd709987dd20cd5947cb5fa93d
Javier Oramas <javiale2000@gmail.com>
2020-12-20 22:34:39 -0500

    making cards a little bit better looking

--- lib/app/modules/home/local_widgets/appliance_cards_list.dart
+++ lib/app/modules/home/local_widgets/appliance_cards_list.dart
@@ -1,3 +1,4 @@
+import 'package:LaLu/app/data/models/user_appliance_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,16 +16,85 @@ class ApplianceCardsList extends StatelessWidget {
                   onLongPress: () {
                     //TODO Remove Functionality
                   },
-                  child: Container(
-                    child: Row(
-                      children: [
-                        Text(e.applianceModel.name),
-                        Text(e.tag),
-                        Text(e.applianceModel.consumption.toString()),
-                      ],
-                    ),
-                  ),
+                  child: createCard(e),
                 ))
             .toList()));
+  }
+
+  createCard(UserApplianceModel e) {
+    return Card(
+      color: Colors.white70,
+      elevation: 6.0,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                "Equipo: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(e.applianceModel.name),
+              Text(
+                " Etiqueta: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(e.tag),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "Consumo Encendido: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(e.applianceModel.consumption.toString() + 'W/h'),
+              Text(
+                " Consumo en Standby: ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(e.applianceModel.standbyConsumption.toString() + 'W/h'),
+            ],
+          ),
+          Row(
+            children: [
+              generateDays(e),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  generateDays(UserApplianceModel e) {
+    List<Widget> children = [];
+
+    for (var i in e.usage.keys) {
+      children.add(Container(
+        margin: EdgeInsets.symmetric(horizontal: 3, vertical: 2),
+        child: Column(
+          children: [
+            Text(
+              i,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(e.usage[i].toString() + ' h'),
+          ],
+        ),
+      ));
+    }
+
+    return Container(
+      child: Row(
+        children: children,
+      ),
+    );
   }
 }
