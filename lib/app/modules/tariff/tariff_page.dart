ec1bf4f6c148e92a4d98409583d336a418440acc
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2021-01-15 22:05:31 -0600

    Create_page_for_electricity_cost_calc

--- lib/app/modules/tariff/tariff_page.dart
+++ lib/app/modules/tariff/tariff_page.dart
@@ -1,8 +1,38 @@
+import 'dart:math';
+
+import 'package:LaLu/app/utils/functions.dart';
 import 'package:flutter/material.dart';
 
-class TariffPage extends StatelessWidget {
+class TariffPage extends StatefulWidget {
+  @override
+  _TariffPageState createState() => _TariffPageState();
+}
+
+class _TariffPageState extends State<TariffPage> {
+  double consumption = 0;
+  double cost = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        TextField(
+          autofocus: true,
+          keyboardType: TextInputType.numberWithOptions(decimal: true),
+          onChanged: (value) => consumption = double.tryParse(value),
+        ),
+        Divider(),
+        RaisedButton(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Icon(Icons.calculate),
+            onPressed: () {
+              setState(() {
+                cost = electricityCost(consumption);
+              });
+            }),
+        Text("$cost")
+      ],
+    );
   }
 }
