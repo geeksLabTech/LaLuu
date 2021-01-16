e73ddc02459bd5f1c1e8e424e0540a2140869a8b
Javier Oramas <javiale2000@gmail.com>
2021-01-15 22:08:03 -0500

    charts

--- lib/app/modules/charts/charts_page.dart
+++ lib/app/modules/charts/charts_page.dart
@@ -1,8 +1,11 @@
+import 'package:LaLu/app/modules/home/local_widgets/stats_list.dart';
 import 'package:flutter/material.dart';
 
 class ChartsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: statsList(),
+    );
   }
 }
