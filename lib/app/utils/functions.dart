02236d9e0a00db112652fbf1a38d828fa0050bc0
Javier Oramas <javiale2000@gmail.com>
2021-01-15 22:57:28 -0500

    fixing consumpion calculator

--- lib/app/utils/functions.dart
+++ lib/app/utils/functions.dart
@@ -3,10 +3,11 @@ import 'constants.dart';
 double electricityCost(double consumption) {
   double cost = 0.0;
 
-  for (var i = 0; i < PRICES.length; i++) {
+  for (var i = 0; i < PRICES.length - 1; i++) {
     var limit = CONSUMPTIONRANGES[CONSUMPTIONRANGES.length - (i + 1)];
     if (consumption > limit) {
-      cost += consumption - limit * PRICES[i];
+      cost += (consumption - limit) * PRICES[PRICES.length - (i + 1)];
+      consumption -= (consumption - limit);
     }
   }
 
