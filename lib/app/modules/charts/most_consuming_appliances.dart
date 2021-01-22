527f16385d9e5f80ea7864d4d9bde739bb09591b
Javier Oramas <javiale2000@gmail.com>
2021-01-22 00:13:57 -0500

    -

--- lib/app/modules/charts/most_consuming_appliances.dart
+++
@@ -1,9 +0,0 @@
-import 'package:LaLu/app/modules/home/home_controller.dart';
-import 'package:get/get.dart';
-
-generateMostConsumingAppliances() {
-  final homeController = Get.find<HomeController>();
-  homeController.userAppliances.sort((a, b) => (a
-      .userApplianceModel.consumptionTotal
-      .compareTo(b.userApplianceModel.consumptionTotal)));
-}
