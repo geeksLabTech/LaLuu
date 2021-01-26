8ea71822c6d122d96eb3dcd47d56f94ea1505bb7
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2021-01-26 15:29:48 -0600

    Create_relevant_info_page

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -3,9 +3,17 @@ const APPLIANCEBOX = 'appliance_box';
 const APPLIANCESELECTEDKEY = 'appliance_selected_key';
 const JSONDBDIRECTORY = 'assets/db/appliance_db.json';
 const JSONDBTESTDIRECTORY = 'assets/db/db_test.json';
+const LOGODIRECTORY = 'assets/logo.png';
 
-const NOMINALTEXT =
-    "Es importante mencionar que el consumo de equipos como aires acondicionados, refrigeradores y freezers puede estar sujeto a errores, dado que se debe utilizar su consumo nominal para mejorar la pecicion del calculo, lamentablemente el consumo nominal varia en dependencia de demasiados factores, incluidos temperatura, area a enfriar etc, por tanto, se aconseja al usuario tomar con cautela las cifras de prediccion de estos equipos";
+const IMPORTANTTEXT = """
+      Los datos de los equipos mostrados en esta aplicación han sido extraidos de distintas páginas de internet. El cálculo del consumo a partir del uso de cada equipo debe ser tomado con cautela, ya que en determinadas circunstancias puede diferir mucho de la realidad. Ejemplo de esto es que un refrigerador solo consume potencia alrededor de 8 horas al día si nunca se desconecta. 
+      A continuación pondremos algunos artículos con los que puede ampliar esta información y poder obtener una estimación mas precisa del consumo.
+      """;
+
+const IMPORTANTURLS = [
+  "https://es.wikihow.com/calcular-los-kilovatios-por-hora?amp=1",
+  "https://procoen.com/9-factores-energia-en-aire-acondicionado/"
+];
 
 const List<double> PRICES = [
   0.33,
